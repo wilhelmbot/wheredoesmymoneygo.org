798671a18e9d271cc9856298caf95ddfdecbc4d4
Rod Wilhelmy <rwilhelmy@gmail.com>
2013-10-01 13:30:30 -0500

    Fixed broken paths on svg icons

--- img/categories/types.js
+++ img/categories/types.js
@@ -6,12 +6,12 @@
 var BubbleTreeStyles = BubbleTreeStyles || {};
 
 BubbleTreeStyles.FondenType = {
-	'agua': { icon: 'fondeninfo_agua.svg', color: '#38a6d5' },
-	'cobijas-colchonetas-e-impermeable': { icon: 'fondeninfo_cobijas.svg', color: '#7b4aa1' },
-	'despensas': { icon: 'fondeninfo_despensa.svg', color: '#40a5a2' },
-	'herramientas': { icon: 'fondeninfo_herramientas.svg', color: '#f5a42c' },
-	'limpieza': { icon: 'fondeninfo_limpieza.svg', color: '#6fdc82' },
-	'medicamentos': { icon: 'fondeninfo_medicamentos.svg', color: '#d53c3a' },
-	'otros': { icon: 'fondeninfo_otros.svg', color: '#aca5a3' }
+	'agua': { icon: 'agua.svg', color: '#38a6d5' },
+	'cobijas-colchonetas-e-impermeable': { icon: 'cobijas.svg', color: '#7b4aa1' },
+	'despensas': { icon: 'despensas.svg', color: '#40a5a2' },
+	'herramientas': { icon: 'herramientas.svg', color: '#f5a42c' },
+	'limpieza': { icon: 'limpieza.svg', color: '#6fdc82' },
+	'medicamentos': { icon: 'medicamentos.svg', color: '#d53c3a' },
+	'otros': { icon: 'otros.svg', color: '#aca5a3' }
 }
 
